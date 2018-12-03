df789c382c3ee3557d0713604b02f0308392812a
q1a2z3w6 <851509034@qq.com>
2018-12-03 20:04:12 +0800

    第一阶段功能基本实现

--- src/main/java/com/lhl/keshang/pub/utils/FileUtils.java
+++ src/main/java/com/lhl/keshang/pub/utils/FileUtils.java
@@ -5,7 +5,10 @@ import com.lhl.keshang.filemanager.pojo.CustomerExcelVo;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
@@ -39,15 +42,14 @@ public class FileUtils {
         Field[] fields = aClass.getFields();
 
 
-
         HSSFWorkbook wb = new HSSFWorkbook();
         int rowNum = 0;
         Sheet sheet = wb.createSheet("记录");
         Row row0 = sheet.createRow(rowNum++);
         //因为场景不同，titil不同，可以在外面写成数组当参数传进来
         String method = "";
-        for(int i=0;i<fields.length;i++){
-            method = "get"+fields[i].getName().toUpperCase()+"_";
+        for (int i = 0; i < fields.length; i++) {
+            method = "get" + fields[i].getName().toUpperCase() + "_";
             Method m = null;
             try {
                 m = aClass.getMethod(method);
@@ -65,23 +67,23 @@ public class FileUtils {
         if (customers != null && customers.size() > 0) {
             for (CustomerExcelVo customer : customers) {
                 Row row = sheet.createRow(rowNum++);
-                for (int i=0;i<fields.length;i++){
+                for (int i = 0; i < fields.length; i++) {
 
                     char[] c = fields[i].getName().toCharArray();
-                    c[0] = (char)(c[0]-32);
+                    c[0] = (char) (c[0] - 32);
                     Method m = null;
                     try {
-                        m = aClass.getMethod("get"+new String(c));
+                        m = aClass.getMethod("get" + new String(c));
                     } catch (NoSuchMethodException e) {
                         e.printStackTrace();
                     }
                     try {
                         String s = null;
                         Object o = m.invoke(customer);
-                        if(o instanceof Date){
+                        if (o instanceof Date) {
                             s = simpleDateFormat.format(o);
-                        }else{
-                            s = o+"";
+                        } else {
+                            s = o + "";
                         }
                         row.createCell(i).setCellValue(s);
                     } catch (IllegalAccessException e) {
@@ -94,7 +96,7 @@ public class FileUtils {
         }
         try {
             wb.write(outputStream);
-            System.out.println("表数据写入到excel表成功,一共写入了"+(rowNum - 1)+"条数据");
+            System.out.println("表数据写入到excel表成功,一共写入了" + (rowNum - 1) + "条数据");
             outputStream.close();
         } catch (IOException e) {
             System.out.println("流关闭异常！");
@@ -108,6 +110,21 @@ public class FileUtils {
             }
         }
         return rowNum - 1;
+    }
+
+    private  String getJarRootPath() throws FileNotFoundException {
+        String path = ResourceUtils.getURL("classpath:").getPath();
+        //=> file:/root/tmp/demo-springboot-0.0.1-SNAPSHOT.jar!/BOOT-INF/classes!/
+//        log.debug("ResourceUtils.getURL(\"classpath:\").getPath() -> " + path);
+        //创建File时会自动处理前缀和jar包路径问题  => /root/tmp
+        File rootFile = new File(path);
+        if (!rootFile.exists()) {
+//            log.info("根目录不存在, 重新创建");
+            rootFile = new File("");
+//            log.info("重新创建的根目录: " + rootFile.getAbsolutePath());
+        }
+//        log.debug("项目根目录: " + rootFile.getAbsolutePath());        //获取的字符串末尾没有分隔符 /
+        return rootFile.getAbsolutePath();
     }
 
 
--- src/test/java/com/lhl/keshang/KeshangApplicationTests.java
+++ src/test/java/com/lhl/keshang/KeshangApplicationTests.java
@@ -4,6 +4,7 @@ import com.lhl.keshang.customer.mapper.CustomerMapper;
 import com.lhl.keshang.customer.mapper.TypeMapper;
 import com.lhl.keshang.customer.pojo.Customer;
 import com.lhl.keshang.customer.pojo.vo.CustomerSelectVo;
+import com.lhl.keshang.pub.utils.FileUtils;
 import com.lhl.keshang.pub.utils.JsonUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileNotFoundException;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -25,10 +27,16 @@ public class KeshangApplicationTests {
     @Test
     public void contextLoads() {
 
-        CustomerSelectVo customerSelectVo = new CustomerSelectVo();
-        customerSelectVo.initParam();
-        List<Customer> customers = customerMapper.findCustomerByVo(customerSelectVo);
-        System.out.println(customers);
+//        CustomerSelectVo customerSelectVo = new CustomerSelectVo();
+//        customerSelectVo.initParam();
+//        List<Customer> customers = customerMapper.findCustomerByVo(customerSelectVo);
+//        System.out.println(customers);
+
+//        try {
+//            System.out.println(FileUtils.getJarRootPath());
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
 
     }
 
